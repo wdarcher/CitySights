52e7aafc5ffdabdcb0ca007ea61d633ec3ea0eb1
Will Archer <wdarcherjr@gmail.com>
2021-07-25 13:33:33 -0500

    Initial Commit

---
+++ City_Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City_Sights
+//
+//  Created by Will Archer on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
