9bc459fbcd877222528411785cf3ea34a4bf7e28
Will Archer <wdarcherjr@gmail.com>
2021-07-25 13:37:07 -0500

    -Set up View, ViewModel, Models Folders - Rename ContentView

--- City_Sights/LaunchView.swift
+++ City_Sights/LaunchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LaunchView.swift
 //  City_Sights
 //
 //  Created by Will Archer on 7/25/21.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LaunchView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LaunchView()
     }
 }
